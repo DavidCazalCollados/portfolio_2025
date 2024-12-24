bcba8ed4e0d7fecfa2efa89d7734ff5c8f8a74ac
David Cazal <da.cazal95@gmail.com>
2024-12-23 20:41:44 -0500

    navbar disposition and font set

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,17 @@
 import React from "react";
+import '../assets/navbar.css'
 
 class Navbar extends React.Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar margin-nav navbar-expand-lg">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Features</a>
-              <a class="nav-link" href="#">Pricing</a>
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <div class="collapse navbar-collapse centered-nav" id="navbarNavAltMarkup">
+            <div class="navbar-nav disposition">
+              <a class="nav-link" href="#">HOME</a>
+              <a class="nav-link" href="#">PROJECTS</a>
+              <a class="nav-link" href="#">ABOUT</a>
+              <a class="nav-link" href="#">CONTACT</a>
             </div>
           </div>
         </div>
