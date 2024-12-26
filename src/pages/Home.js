ed15b1c33a64a7196ff0cf894dd988b074e9723e
David Cazal <da.cazal95@gmail.com>
2024-12-26 16:06:22 -0500

    home page initialized

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,16 @@ import "../assets/home.css"
 function Home() {
   return (
     <div class="home-disposition">
-      <div class="home-left"><div>bonjour</div></div>
-      <div class="home-right"></div>
+      <div class="home-left">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Dicta in distinctio earum totam, exercitationem delectus dignissimos
+          accusantium itaque minus voluptate expedita dolore quasi ullam hic
+          eligendi alias vel, deserunt id!
+        </p>
+      </div>
+      <div class="home-right">
+
+      </div>
     </div>
   );
 }
