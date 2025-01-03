864f12d14a7a428a30dd4b3e9c94a3f5c50e6c7c
David Cazal <da.cazal95@gmail.com>
2025-01-02 21:39:09 -0500

    text finally scrolling

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -7,69 +7,26 @@ class Sidebar extends React.Component {
   render() {
     const info = [
       { label: 'GitHub', url: 'https://github.com/DavidCazalCollados' },
-      { label: 'LinkedIn', url: 'www.linkedin.com/in/david-cazal' },
+      { label: 'LinkedIn', url: 'https://linkedin.com/in/david-cazal' },
       { label: 'Email', url: 'mailto:da.cazal95@gmail.com' },
       { label: 'Téléphone', url: 'tel:+14388892467' }
     ];
     return (
       <div className="leftnav">
-        <motion.div
-        className="scrolling-wrapper"
-        animate={{ y: ['-100%', '200%'] }}
-        transition={{
-          duration: 10,
-          repeat: Infinity,
-          ease: 'linear',
-        }}
-        >
-          <div className="scrolling-text">
-            {info.map((item, index) => (
-                <a className="scroll-item" key={index} href={item.url} target="_blank" rel="noopener noreferrer">{item.label}</a>
-            ))}
-          </div>
-        </motion.div>
-
-        <motion.div
-        className="scrolling-wrapper"
-        animate={{ y: ['-100%', '200%'] }}
-        transition={{
-          duration: 10,
-          repeat: Infinity,
-          ease: 'linear',
-          delay: 4,
-        }}
-        >
-          <div className="scrolling-text">
-            {info.map((item, index) => (
-                <a className="scroll-item" key={index} href={item.url} target="_blank" rel="noopener noreferrer">{item.label}</a>
-            ))}
-          </div>
-        </motion.div>
-        {/* <motion.div
-          className="scrolling-wrapper"
-          animate={{ y: ['-100%', '100%'] }}
-          transition={{
-            duration: info.length,
-            repeat: Infinity,
-            ease: 'linear',
-          }}
-        >
-          <div className="scrolling-text">
+        <ul>
           {info.map((item, index) => (
-            <span key={index} className="scroll-item">
-              <a href={item.url} target="_blank" rel="noopener noreferrer">{item.label}</a>
-            </span>
+            <li>
+              <a className="scroll-item" key={index} href={item.url} target="_blank" rel="noopener noreferrer">{item.label}</a>
+            </li>
           ))}
-          </div>
-
-          <div className="scrolling-text">
+        </ul>
+        <ul aria-hidden="true">
           {info.map((item, index) => (
-            <span key={index} className="scroll-item">
-              <a href={item.url} target="_blank" rel="noopener noreferrer">{item.label}</a>
-            </span>
+            <li>
+              <a className="scroll-item" key={index} href={item.url} target="_blank" rel="noopener noreferrer">{item.label}</a>
+            </li>
           ))}
-          </div>
-        </motion.div> */}
+        </ul>
       </div>
     )
   }
