7dc162425c24f559a4e467a8828663c2986b664f
David Cazal <da.cazal95@gmail.com>
2025-02-11 13:12:30 -0500

    basic navbar responsive

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -25,12 +25,18 @@ function Navbar() {
               <Link className="button-link" to="/about">ABOUT</Link>
               <Link className="button-link" to="/contact">CONTACT</Link>
             </div>
-            <div className="hamburger">
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 50 50">
+            <div className="hamburger" onClick={toggleMenu}>
+              <svg className="hamburger-icon" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 50 50">
                 <path
                   d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"
                 />
               </svg>
+              <div className={`dropdown-menu ${menuOpen ? 'open' : ''}`}>
+                <Link className="button-link" to="/">HOME</Link>
+                <Link className="button-link" to="/projects">PROJECTS</Link>
+                <Link className="button-link" to="/about">ABOUT</Link>
+                <Link className="button-link" to="/contact">CONTACT</Link>
+              </div>
             </div>
           </nav>
         </motion.div>
