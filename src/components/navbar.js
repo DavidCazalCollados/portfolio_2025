86028d30a3669b696aac07e74039934f6e2d4e98
David Cazal <da.cazal95@gmail.com>
2025-02-11 17:34:58 -0500

    bug fixed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -25,17 +25,17 @@ function Navbar() {
               <Link className="button-link" to="/about">ABOUT</Link>
               <Link className="button-link" to="/contact">CONTACT</Link>
             </div>
-            <div className="hamburger" onClick={toggleMenu}>
-              <svg className="hamburger-icon" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 50 50">
+            <div className="hamburger">
+              <svg className="hamburger-icon" onClick={toggleMenu} xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 50 50">
                 <path
                   d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"
                 />
               </svg>
               <div className={`dropdown-menu ${menuOpen ? 'open' : ''}`}>
-                <Link className="button-link" to="/">HOME</Link>
-                <Link className="button-link" to="/projects">PROJECTS</Link>
-                <Link className="button-link" to="/about">ABOUT</Link>
-                <Link className="button-link" to="/contact">CONTACT</Link>
+                <Link className={`button-link ${menuOpen ? 'show' : ''}`} to="/">HOME</Link>
+                <Link className={`button-link ${menuOpen ? 'show' : ''}`} to="/projects">PROJECTS</Link>
+                <Link className={`button-link ${menuOpen ? 'show' : ''}`} to="/about">ABOUT</Link>
+                <Link className={`button-link ${menuOpen ? 'show' : ''}`} to="/contact">CONTACT</Link>
               </div>
             </div>
           </nav>
